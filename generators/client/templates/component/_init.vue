97e49a58605f76191b61826a60ea6e20d762b039
everthis <wwwaap@gmail.com>
2017-09-22 18:45:17 +0800

    modify webpack config

---
+++ generators/client/templates/component/_init.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    init.
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {}
+        },
+        mounted () {
+        },
+        beforeDestroy () {
+        },
+        methods: {
+        }
+    }
+</script>
