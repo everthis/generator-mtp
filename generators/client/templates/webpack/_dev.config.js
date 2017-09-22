de951c8a5b65a668161cd01175938aed33c2a46d
everthis <wwwaap@gmail.com>
2017-09-22 18:44:53 +0800

    modify webpack config

--- generators/client/templates/webpack/_dev.config.js
+++ generators/client/templates/webpack/_dev.config.js
@@ -2,37 +2,35 @@
  * webpack development config
  * @type {[Object]}
  */
-let merge = require('webpack-merge');
-let webpack = require('webpack');
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
-let baseConfig = require('./base.config').defaults;
-let devConfig = {};
+let merge = require('webpack-merge')
+let webpack = require('webpack')
+let ExtractTextPlugin = require('extract-text-webpack-plugin')
+let baseConfig = require('./base.config').defaults
+let devConfig = {}
 
 /**
  * find out which loader is causing deprecation warning.
  */
-process.traceDeprecation = true;
+process.traceDeprecation = true
 
 devConfig = merge.smartStrategy({
-    'entry': 'prepend',
-    'module.loaders': 'prepend'
+  entry: 'prepend',
+  'module.loaders': 'prepend'
 })({}, baseConfig, {
-    output: {
-    	filename: '[name].js',
-    	chunkFilename: '[id].chunk.js',
-    },
-    performance: {
-      hints: false
-    },
-    devServer: {
-      /*
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+  performance: {
+    hints: false
+  },
+  devServer: {
+    /*
        * process invalid host header.
        */
-      disableHostCheck: true,
-      sockjsPrefix: '/ws/<%= s || moduleName -%>'
-    },
-    plugins: [
-	    new ExtractTextPlugin("[name].css")
-    ]
-});
-module.exports = devConfig;
+    disableHostCheck: true,
+    sockjsPrefix: '/ws/webpack/<%= s || moduleName -%>'
+  },
+  plugins: [new ExtractTextPlugin('[name].css')]
+})
+module.exports = devConfig
